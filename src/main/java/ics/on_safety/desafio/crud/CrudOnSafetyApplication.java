65e343d14a62d689dc3ea10f74021db53e8ee112
Ivan Santos <ivansantos.ronem@gmail.com>
2024-07-19 15:20:48 -0300

    feat: montagem e config ambiente

---
+++ src/main/java/ics/on_safety/desafio/crud/CrudOnSafetyApplication.java
@@ -0,0 +1,13 @@
+package ics.on_safety.desafio.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOnSafetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOnSafetyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ics/on_safety/desafio/crud/CrudOnSafetyApplicationTests.java
@@ -0,0 +1,13 @@
+package ics.on_safety.desafio.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOnSafetyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
