e0d64be2274336b0124bb726c6a5f26a7021a383
Ivan Santos <ivansantos.ronem@gmail.com>
2024-08-07 11:34:56 -0300

    feat: add restclient

--- src/main/java/ics/on_safety/desafio/crud/CrudOnSafetyApplication.java
+++ src/main/java/ics/on_safety/desafio/crud/CrudOnSafetyApplication.java
@@ -1,16 +1,15 @@
 package ics.on_safety.desafio.crud;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 
-@Slf4j
+
 @SpringBootApplication
 public class CrudOnSafetyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrudOnSafetyApplication.class, args);
-        log.info("Application UP {}", HttpStatus.OK);
+        System.out.println("Application UP [" + HttpStatus.OK + "]");
     }
 }
--- src/main/java/ics/on_safety/desafio/crud/config/RestClientConfiguration.java
+++ src/main/java/ics/on_safety/desafio/crud/config/RestClientConfiguration.java
@@ -2,14 +2,13 @@ package ics.on_safety.desafio.crud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-
+import org.springframework.web.client.RestClient;
 
 @Configuration
-public class RestTemplateConfiguration {
+public class RestClientConfiguration {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestClient restClient() {
+        return RestClient.builder().build();
     }
 }
